4be2d51418e0ecb9b1ae04dc68a01e94579c2e6e
Ospino <ospino.p@gmail.com>
2019-10-01 22:14:51 +0200

    Initial celery modules
    Signed-off-by: Ospino <ospino.p@gmail.com>

---
+++ scrapper/__init__.py
@@ -0,0 +1,6 @@
+from celery import Celery
+
+app = Celery(
+    'tasks',
+    broker='redis://localhost'
+)
---
+++ submitter.py
@@ -0,0 +1,6 @@
+from .scrapper import do_work
+
+if __name__ == '__main__':
+    for i in range(10):
+        result = do_work.delay(i)
+        print('task submitted' + str(i))
